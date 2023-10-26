cce88e2dc4591a37b2ff01d3408a5b23b0bb9ad8
pierre merlaud <pmerlaud007@gmail.com>
2023-10-26 16:50:57 +0200

    home page base ok

---
+++ actions/getLatestPhotos.js
@@ -0,0 +1,15 @@
+"use server";
+import Photo from "@/models/photoModel";
+
+export async function getLatestPhotos() {
+  try {
+    const photos = await Photo.find().sort("-createdAt").limit(6);
+    const resources = photos.map((photo) => ({
+      ...photo._doc,
+      _id: photo._id.toString(),
+    }));
+    return resources;
+  } catch (error) {
+    return { errMsg: error.message };
+  }
+}
