7596f47bce0668dabd36af39577916282e19d8f5
pierre merlaud <pmerlaud007@gmail.com>
2023-10-30 11:52:40 +0100

    first branch push test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import "./globals.css";
+import "./styles/globals.css";
+import "./styles/gallery.css";
 import { Inter } from "next/font/google";
 import connectDB from "@/utils/database";
 import Navbar from "./components/Navbar";
